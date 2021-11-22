91d5254878ba1d4ec296ef66c1465bbdf24a17fd
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-22 17:33:53 +0100

    watermark works in center

--- app2.js
+++ app2.js
@@ -1,3 +1,5 @@
+const Jimp = require('jimp');
+
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   const image = await Jimp.read(inputFile);
   const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
@@ -10,6 +12,8 @@ const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
   await image.quality(100).writeAsync(outputFile);
 };
+
+addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'XXX')
 
 // const Jimp = require('jimp');
 
